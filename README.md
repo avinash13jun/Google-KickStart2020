368fa0bcfc680cbdc32173e63043f5da7ee200ee
Avinash Kumar <Avinash.Kumar5@gds.ey.com>
2020-10-28 08:08:42 +0530

    Added Round G Kick Start Solution

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Google-KickStart2020
+
+This repository contains the solutions for Google Kick Start problems using C#. The folder structure is designed as below -
+
+Google-KickStart2020
+			-> Round folder
+						-> Problems
---
+++ Round G/Kick_Start/Google_KS_Round_G_2020/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Kick_Start
+{
+    class Program
+    {
+        public const string K = "KICK";
+        public const string S = "START";
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int testCase = 1; testCase <= t; testCase++)
+            {
+                int matchCount = CheckStart(Console.ReadLine());
+                Console.WriteLine($"Case #{testCase}: {matchCount}");
+            }
+        }
+
+        public static int CheckStart(string input)
+        {
+            int count = 0;
+            int matchfound = -1;
+            int previousMatch = 0;
+            int previousCount = 0;
+            for (int i = 0; i <= input.Length;)
+            {
+                matchfound = input.IndexOf(S, i);
+                if (matchfound > -1)
+                {
+                    previousCount += CheckKickCount(input, matchfound, previousMatch);
+                    count += previousCount;
+                    i = matchfound + 1;
+                    previousMatch = matchfound;
+                }
+                else
+                    break;
+            }
+
+            return count;
+        }
+
+        public static int CheckKickCount(string input, int index, int previousMatch)
+        {
+            int count = 0;
+            int matchFound = -1;
+            for (int i = previousMatch; i < index;)
+            {
+                matchFound = input.IndexOf(K, i, index - i);
+                if (matchFound > -1)
+                {
+                    count++;
+                    i = matchFound + 1;
+                }
+                else
+                    break;
+            }
+
+            return count;
+        }
+    }
+}
